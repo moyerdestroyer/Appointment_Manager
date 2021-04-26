ca8633138c1e60b23c9d11a6afb9f487c7f97177
ryan <moyerdestroyer@gmail.com>
2021-04-26 15:57:10 -0500

    All Views and Controllers

--- src/Controllers/CustomerViewController.java
+++ src/Controllers/CustomerViewController.java
@@ -2,12 +2,19 @@ package Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomerViewController {
 
@@ -80,8 +87,13 @@ public class CustomerViewController {
     }
 
     @FXML
-    void AppointmentViewAction(ActionEvent event) {
-
+    void AppointmentViewAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Appointment_View.fxml"));
+        Parent root = (Parent) loader.load();
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setTitle("Appointment View");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     @FXML
